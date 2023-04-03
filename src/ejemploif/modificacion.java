53b21e8dc0ceae4a86cd0dc96768c3f7d3472cdd
34622 <34622@CelestePC>
2023-04-03 11:07:39 +0200

    Agregación nueva clase "modificación"

---
+++ src/ejemploif/modificacion.java
@@ -0,0 +1,13 @@
+
+package ejemploif;
+
+
+public class modificacion {
+    public void aseate(){
+        System.out.println("El pinguino se esta aseando.");
+    }
+    
+    public void vuela(){
+        System.out.println("El pinguino vuela.");
+    }
+}
